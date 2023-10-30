0d89e04d4dc7df19c1d1b03c3447b5a79afcafd9
Erfan-fn <erfan.farahmandnezhad2003@gmail.com>
2023-10-30 23:04:48 +0330

    create class Node
    that has row,column,element,NextInRow,NextIncolumn

---
+++ src/Matrix.java
@@ -0,0 +1,56 @@
+        public static class Node {
+
+            private int row;
+            private int column;
+            private int element;
+            private Node nextInRow;
+            private Node nextInColumn;
+
+            public Node(int element,int row,int column) {
+                this.element = element;
+                this.row=row;
+                this.column=column;
+
+            }
+
+            public int getRow() {
+                return row;
+            }
+
+            public void setRow(int row) {
+                this.row = row;
+            }
+
+            public int getColumn() {
+                return column;
+            }
+
+            public void setColumn(int column) {
+                this.column = column;
+            }
+
+            public int getElement() {
+                return element;
+            }
+
+            public void setElement(int element) {
+                this.element = element;
+            }
+
+            public Node getNextInRow() {
+                return nextInRow;
+            }
+
+            public void setNextInRow(Node nextInRow) {
+                this.nextInRow = nextInRow;
+            }
+
+            public Node getNextInColumn() {
+                return nextInColumn;
+            }
+
+            public void setNextInColumn(Node nextInColumn) {
+                this.nextInColumn = nextInColumn;
+            }
+
+        }
