07aac1e7602d6d853161c48ce58094d388463d0a
Erfan-fn <erfan.farahmandnezhad2003@gmail.com>
2023-10-30 23:09:49 +0330

    complete all kinds of add Function
    such as addlast, addfirst and in the middle both for Column and Row lists

--- src/Matrix.java
+++ src/Matrix.java
@@ -73,4 +73,111 @@
             return this.tail;
         }
 
+        public void addFirstInRow(int element,int row,int column) {
+            Node newNode = new Node(element,row,column);
+
+            if (this.size == 0) {
+                head = newNode;
+                newNode.nextInRow = null;
+                tail = newNode;
+            } else {
+                newNode.nextInRow = head;
+                head = newNode;
+            }
+            size++;
+        }
+
+        public void addFirstInCol(int element,int row,int column) {
+            Node newNode = new Node(element,row,column);
+
+            if (this.size == 0) {
+                head = newNode;
+                newNode.nextInColumn = null;
+                tail = newNode;
+            } else {
+                newNode.nextInColumn = head;
+                head = newNode;
+            }
+            size++;
+        }
+
+        public void addLastInRow(int element,int row,int column) {
+            Node newNode = new Node(element,row,column);
+            if (this.size==0){
+                addFirstInRow(element,row,column);
+            }
+            else
+            {
+                this.tail.nextInRow = newNode;
+                tail = newNode;
+                size++;
+            }
+        }
+
+        public void addLastInCol(int element,int row,int column) {
+            Node newNode = new Node(element,row,column);
+            if (this.size==0){
+                addFirstInCol(element,row,column);
+            }
+            else
+            {
+                this.tail.nextInColumn = newNode;
+                tail = newNode;
+                size++;
+            }
+        }
+        public void add(int element,int row,int column,boolean isRow) {
+
+            if (isRow)
+            {
+                Node current = new Node(element,row,column);
+                Node previousRow=this.getPrevInRowWithCol(column);
+                Node nextRow=this.getNextInRowWithCol(column);
+
+                if (previousRow==null && nextRow!=null) {
+                    addFirstInRow(element,row,column);
+                }
+                else if (previousRow!=null && nextRow==null)
+                {
+                    addLastInRow(element,row,column);
+                }
+
+                 if(previousRow!=null && nextRow!=null){
+                    Node newNode = new Node(element,row,column);
+                    previousRow.nextInRow = newNode;
+                    newNode.nextInRow = current;
+                    size++;
+                }
+
+                if(this.size==0){
+                    addFirstInRow(element,row,column);
+                }
+            }
+            else
+            {
+                Node current = new Node(element,row,column);
+                Node previousCol=this.getPrevInColWithRow(row);
+                Node nextCol=this.getNextInColWithRow(row);
+
+                if (previousCol==null && nextCol!=null) {
+                    addFirstInCol(element,row,column);
+                }
+                else if (previousCol!=null && nextCol==null)
+                {
+                    addLastInCol(element,row,column);
+                }
+
+                if(previousCol!=null && nextCol!=null){
+                    Node newNode = new Node(element,row,column);
+                    previousCol.nextInColumn = newNode;
+                    newNode.nextInColumn = current;
+                    size++;
+                }
+
+                if(this.size==0){
+                    addFirstInCol(element,row,column);
+                }
+            }
+
+        }
     }
